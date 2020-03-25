3d5067d589971b12f8b4f112be5aff71c8a162d4
NMenand <nicolasmenand@gmail.com>
2020-03-24 17:22:23 -0700

    Added psl prepare bash script

---
+++ prepare_psl.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+readonly BASE_DIR=$(realpath "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )")
+
+# $1 is the dat file. Assumed to be entire data to be split where a line is a tab separated triple
+# $2 is the config file
+
+python3 ${BASE_DIR}/scripts/prepare_psl.py $1 $2
+
