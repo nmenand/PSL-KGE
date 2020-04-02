e34079b53276e9dc0e3b5ea0bd5a7360657af339
cfpryor <cfpryor@ucsc.edu>
2020-04-02 16:12:50 -0700

    Updating psl_prep.

--- scripts/psl_prep.py
+++ scripts/psl_prep.py
@@ -4,6 +4,8 @@ import json
 import os
 import shutil
 import sys
+	
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
 DATA = "data"
 DATA_FILE = "data.txt"
@@ -19,9 +21,14 @@ ENTITYDIM = "entitydim"
 RELATIONDIM = "relationdim"
 TARGET = "_target.txt"
 
+# ENTITY_1 = 1
+# ENTITY_2 = 3
+# RELATION = 2
+
 # Writes a list of data to a file
 def write_data(data, file_path):
 	with open(file_path, 'w+') as out_file:
+		# (TODO) out_file.write('\n'.join(["\t".join(example) for example in data]))
 		out_file.write('\n'.join(data))
 
 # Return list of triples from filename
@@ -43,7 +50,9 @@ def map_constituents(triple_list):
 	for triple in triple_list:
 		if not triple[0] in entity_map:
 			entity_map[triple[0]] = str(entity_count)
-			entity_count += 1
+			# (TODO)
+         #triple[0] = str(entity_count)
+         entity_count += 1
 		if not triple[1] in relation_map:
 			relation_map[triple[1]] = str(relation_count)
 			relation_count += 1
@@ -70,13 +79,16 @@ def get_split_count(splits_dir):
 
 # Note: Create PSL rules
 def main(dataset_name, dim_num):
-	current_dir = os.path.dirname(os.path.realpath(__file__))
+	# (TODO) change to a constant
+   current_dir = os.path.dirname(os.path.realpath(__file__))
 	dataset_dir = os.path.join(os.path.dirname(current_dir), dataset_name)
 	data_dir = os.path.join(dataset_dir, DATA)
 	splits_dir = os.path.join(dataset_dir, SPLITS)
 
+   # (TODO) raw_data
 	full_triple_list = load_helper(os.path.join(dataset_dir, DATA_FILE))
-	entity_map, relation_map = map_constituents(full_triple_list)
+	# (TODO) Feed back data
+   entity_map, relation_map = map_constituents(full_triple_list)
 
 	# Create or replace(if exists) data_dir
 	if os.path.exists(data_dir):
@@ -93,7 +105,8 @@ def main(dataset_name, dim_num):
 
 	# Todo: Get split count from config file
 	for split_num in range(0, get_split_count(splits_dir)):
-		cur_split = SPLIT + str(split_num)
+		# (TODO) Change to just number for split name
+      cur_split = SPLIT + str(split_num)
 		data_split_dir = os.path.join(data_dir, cur_split)
 		raw_split_dir = os.path.join(dataset_dir, SPLITS, cur_split)
 
