b759a29cef2d73843261e68077c6751ef4cb5598
NMenand <nicolasmenand@gmail.com>
2020-04-29 00:22:28 -0700

    Simplified Eval.py

--- scripts/eval.py
+++ scripts/eval.py
@@ -23,8 +23,11 @@ DIMENSIONS = "dimensions"
 MAP_INPUT = "map_input"
 TEST_FILE = "/test.txt"
 DATASET = "dataset"
-
 SPLIT_NO = "0"
+
+ENTITY_1 = 0
+RELATION = 1
+ENTITY_2 = 2
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 MAPE_DIR = os.path.join(os.path.dirname(BASE_DIR), ENTITY_MAP)
@@ -33,6 +36,8 @@ MAPR_DIR = os.path.join(os.path.dirname(BASE_DIR), RELATION_MAP)
 ENTITY_DIR = os.path.join(os.path.dirname(BASE_DIR), ENTITY_DIM)
 RELATION_DIR = os.path.join(os.path.dirname(BASE_DIR), RELATION_DIM)
 DATA_DIR = os.path.dirname(BASE_DIR)
+TRUE_DIR = os.path.join(DATA_DIR, PSL_DATA_DIR + SPLIT_NO + TRUEBLOCK_DIR)
+FALSE_DIR = os.path.join(DATA_DIR, PSL_DATA_DIR + SPLIT_NO + FALSEBLOCK_DIR)
 
 def load_mappings(file_name, key, value):
     map = {}
@@ -54,17 +59,12 @@ def write_data(data, file_path):
 def eval_triple(mapped_e1 , mapped_e2, mapped_rel, dimensions, ent_embeddings, rel_embeddings):
     sum = 0
     for dim in range(1, dimensions+1):
-        try:
-            e1_num = ent_embeddings[dim-1][mapped_e1]
-            e2_num = ent_embeddings[dim-1][mapped_e2]
-            rel_num = rel_embeddings[dim-1][mapped_rel]
-        except Exception:
-            e1_num = 1
-            e2_num = 1
-            rel_num = 0
-        value = float(e1_num) + float(rel_num) - float(e2_num)
+        e1_num = float(ent_embeddings[dim-1][mapped_e1])
+        e2_num = float(ent_embeddings[dim-1][mapped_e2])
+        rel_num = float(rel_embeddings[dim-1][mapped_rel])
+        value = e1_num + rel_num - e2_num
 
-        sum += value*value
+        sum += value**2
     return math.sqrt(sum)
 
 # ent_list is a list mapped entities where each entity is of type string
@@ -97,8 +97,6 @@ def load_data(config):
     return data, entity_list, set_of_data
 
 def test_all(config, entity_mapping, relation_mapping):
-    TRUE_DIR = os.path.join(DATA_DIR, PSL_DATA_DIR + SPLIT_NO + TRUEBLOCK_DIR)
-    FALSE_DIR = os.path.join(DATA_DIR, PSL_DATA_DIR + SPLIT_NO + FALSEBLOCK_DIR)
 
     dimensions = config[DIMENSIONS]
     pos_sum = 0
@@ -117,9 +115,9 @@ def test_all(config, entity_mapping, relation_mapping):
         rel_embeddings.append(load_mappings(RELATION_DIR + str(dim) + TXT, 0, 1))
 
     for triple in triples:
-        mapped_e1 = triple[0]
-        mapped_e2 = triple[2]
-        mapped_rel = triple[1]
+        mapped_e1 = triple[ENTITY_1]
+        mapped_e2 = triple[ENTITY_2]
+        mapped_rel = triple[RELATION]
 
         eval = eval_triple(mapped_e1 , mapped_e2, mapped_rel, dimensions, ent_embeddings, rel_embeddings)
         pos_sum+= eval
@@ -139,9 +137,9 @@ def test_all(config, entity_mapping, relation_mapping):
     trip_fd.close()
 
     for triple in triples:
-        mapped_e1 = triple[0]
-        mapped_e2 = triple[2]
-        mapped_rel = triple[1]
+        mapped_e1 = triple[ENTITY_1]
+        mapped_e2 = triple[ENTITY_2]
+        mapped_rel = triple[RELATION]
 
         eval = eval_triple(mapped_e1 , mapped_e2, mapped_rel, dimensions, ent_embeddings, rel_embeddings)
         neg_sum += eval
@@ -151,66 +149,27 @@ def test_all(config, entity_mapping, relation_mapping):
     print("Negative triples total sum is: " +str(neg_sum))
     print("Negative triples average is: " +str(neg_sum/len(triples)))
 
-def main(config, test_triple, flag_eval_all):
+def main(config):
 
     data, entity_list, set_of_data = load_data(config)
-    (e1, rel, e2) = test_triple
     dimensions = config[DIMENSIONS]
 
     entity_mapping = load_mappings(MAPE_DIR, 1, 0)
     relation_mapping = load_mappings(MAPR_DIR, 1, 0)
 
-    if(flag_eval_all == 1):
-        if test_triple in set_of_data:
-            print("Valid Triple")
-        else:
-            print("Corrupted Triple")
-
-        if(config[MAP_INPUT] == 1):
-            entity_mapping = load_mappings(MAPE_DIR, 1, 0)
-            relation_mapping = load_mappings(MAPR_DIR, 1, 0)
-
-            mapped_e1 = entity_mapping[e1]
-            mapped_e2 = entity_mapping[e2]
-            mapped_rel = relation_mapping[rel]
-        else:
-            mapped_e1 = e1
-            mapped_e2 = e2
-            mapped_rel = rel
-
-        # Get entity and relation embeddings for each dimension
-        ent_embeddings = []
-        rel_embeddings = []
-
-        # ent_embeddings is a list of dictionaries
-        for dim in range(1, dimensions+1):
-            ent_embeddings.append(load_mappings(ENTITY_DIR + str(dim) + TXT, 0, 1))
-            rel_embeddings.append(load_mappings(RELATION_DIR + str(dim) + TXT, 0, 1))
-
-        eval = eval_triple(mapped_e1 , mapped_e2, mapped_rel, dimensions, ent_embeddings, rel_embeddings)
-        print("TransE Evaluation Function : " + str(eval) + "\n")
-
     test_all(config, entity_mapping, relation_mapping)
 
 def _load_args(args):
     executable = args.pop(0)
-    if (len(args) != 1 and len(args) != 4) or ({'h','help'} & {arg.lower().strip().replace('-', '') for arg in args}):
+    if (len(args) != 1 or ({'h','help'} & {arg.lower().strip().replace('-', '') for arg in args})):
         print("USAGE: python3 %s <config.json> original_entity1 original_relation original_entity2" % executable, file = sys.stderr)
         sys.exit(1)
 
     config_file = args.pop(0)
     with open(config_file, 'r') as config_fd:
         config = json.load(config_fd)
-        if len(args) == 0:
-            triple = (0,0,0)
-            return config, triple, 0
-        else:
-            entity1 = args.pop(0)
-            relation = args.pop(0)
-            entity2 = args.pop(0)
-            triple = (str(entity1), str(relation), str(entity2))
-            return config, triple, 1
+        return config
 
 if __name__ == '__main__':
-    config, test_triple, eval_all = _load_args(sys.argv)
-    main(config, test_triple, eval_all)
+    config = _load_args(sys.argv)
+    main(config)
